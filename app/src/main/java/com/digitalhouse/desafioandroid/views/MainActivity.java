649a6cb098ffc9d5f6ce33a99f85026e813a6af9
Inghridy Santos <60369256+Marvelmobile@users.noreply.github.com>
2020-02-23 12:51:19 -0300

    Splash's feature and layout development

---
+++ app/src/main/java/com/digitalhouse/desafioandroid/views/MainActivity.java
@@ -0,0 +1,20 @@
+package com.digitalhouse.desafioandroid.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.digitalhouse.desafioandroid.R;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
