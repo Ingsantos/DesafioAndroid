4bdffb3455b91253da61f2f66e1a01d0cf75d328
Inghridy Santos <60369256+Marvelmobile@users.noreply.github.com>
2020-02-24 16:41:41 -0300

    Register's layout

--- app/src/main/java/com/digitalhouse/desafioandroid/views/RegisterActivity.java
+++ app/src/main/java/com/digitalhouse/desafioandroid/views/RegisterActivity.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 
 import com.digitalhouse.desafioandroid.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_register);
     }
 }
