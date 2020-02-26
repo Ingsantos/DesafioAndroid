72c85beab3e3a6f2fdd5837740c9f7c633f2183f
Ingsantos <khllnghrd@gmail.com>
2020-02-26 19:48:40 -0300

    Menu's item layout and feature done

--- app/src/main/java/com/digitalhouse/desafioandroid/views/MainActivity.java
+++ app/src/main/java/com/digitalhouse/desafioandroid/views/MainActivity.java
@@ -1,10 +1,14 @@
 package com.digitalhouse.desafioandroid.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.digitalhouse.desafioandroid.R;
 import java.util.Timer;
 
@@ -22,5 +26,22 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, fragment).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.profile) {
+            startActivity(new Intent(MainActivity.this, SplashActivity.class));
+            finishAffinity();
+        }
+
+        return true;
     }
 }
